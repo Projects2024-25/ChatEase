c8922fd426866684894f243a302036509dacb6b2
Anurag <anuragsaha9183@gmail.com>
2024-10-30 11:41:44 +0530

    sign in xml file update new ui

--- app/src/main/java/com/example/chatease/activities/SignInActivity.kt
+++ app/src/main/java/com/example/chatease/activities/SignInActivity.kt
@@ -24,95 +24,95 @@ class SignInActivity : AppCompatActivity() {
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
 
         //To be able Check if the user is coming from the SignUpActivity
-        val fromSignUp = intent.getBooleanExtra("fromSignUp",false)
-
-        //Checking if the user is already logged in AND is not coming from the SignUpActivity
-        //Open the MainActivity and close the SignInActivity
-        if(auth.currentUser != null && !fromSignUp) {
-            startActivity(Intent(this@SignInActivity, MainActivity::class.java))
-            finish()
-        }
-
-        //SignIn Button
-        binding.buttonSignIn.setOnClickListener{
-            isLoading(true)
-            if(!isValidSignUp()) {
-                return@setOnClickListener
-            }
-            signIn()
-        }
-
-        //A textView of "Don't have an Account? Sign Up" using as a Button
-        binding.textViewSignUp.setOnClickListener {
-            Log.d("test","test")
-            val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-            startActivity(intent)
-        }
-    }
-
-
-    private fun showToast(message: String) {
-        Toast.makeText(this@SignInActivity, message, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun isValidSignUp(): Boolean {
-        if (binding.editTextEmail.text.isNullOrEmpty()) {
-            //if Email editText is empty
-            isLoading(false)
-            binding.editLayoutEmail.error = "Please fill the email field"
-            return false
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.editTextEmail.text.toString()).matches()) {
-            //if Email is not valid
-            isLoading(false)
-            binding.editLayoutEmail.error = "Not a valid email"
-            return false
-        } else if (binding.editTextPassword.text.isNullOrEmpty()) {
-            //if password editText is empty
-            isLoading(false)
-            binding.editLayoutEmail.error = null
-            binding.editLayoutPassword.error = "Please fill the password field"
-            return false
-        } else if (binding.editTextPassword.text.toString().length < 6) {
-            //if password is less than 6 letters, forced by Firebase Authentication Service
-            isLoading(false)
-            binding.editLayoutEmail.error = null
-            binding.editLayoutPassword.error = "Password should contain atleast 6 letters"
-        }
-        return true
-    }
-
-    private fun isLoading(bool: Boolean) {
-        //This is a Rounded progressBar
-        if(bool) {
-            binding.progressBar.visibility = View.VISIBLE
-            binding.buttonSignIn.visibility = View.INVISIBLE
-        } else {
-            binding.progressBar.visibility = View.INVISIBLE
-            binding.buttonSignIn.visibility = View.VISIBLE
-        }
-    }
-
-    private fun signIn() {
-        //Trying to Authenticate the user by creating the id
-        auth.signInWithEmailAndPassword(binding.editTextEmail.text.toString().trim(), binding.editTextPassword.text.toString().trim())
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    isLoading(false)
-                    startActivity(Intent(this@SignInActivity, MainActivity::class.java))
-                    finish()
-                } else {
-                    isLoading(false)
-                    showToast("Login failed")
-                    Log.d("SignIpError",task.exception.toString())
-                }
-            }
-    }
+//        val fromSignUp = intent.getBooleanExtra("fromSignUp",false)
+//
+//        //Checking if the user is already logged in AND is not coming from the SignUpActivity
+//        //Open the MainActivity and close the SignInActivity
+//        if(auth.currentUser != null && !fromSignUp) {
+//            startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+//            finish()
+//        }
+//
+//        //SignIn Button
+//        binding.buttonSignIn.setOnClickListener{
+//            isLoading(true)
+//            if(!isValidSignUp()) {
+//                return@setOnClickListener
+//            }
+//            signIn()
+//        }
+//
+//        //A textView of "Don't have an Account? Sign Up" using as a Button
+//        binding.textViewSignUp.setOnClickListener {
+//            Log.d("test","test")
+//            val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
+//            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+//            startActivity(intent)
+//        }
+   }
+//
+//
+//    private fun showToast(message: String) {
+//        Toast.makeText(this@SignInActivity, message, Toast.LENGTH_SHORT).show()
+//    }
+//
+//    private fun isValidSignUp(): Boolean {
+//        if (binding.editTextEmail.text.isNullOrEmpty()) {
+//            //if Email editText is empty
+//            isLoading(false)
+//            binding.editLayoutEmail.error = "Please fill the email field"
+//            return false
+//        } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.editTextEmail.text.toString()).matches()) {
+//            //if Email is not valid
+//            isLoading(false)
+//            binding.editLayoutEmail.error = "Not a valid email"
+//            return false
+//        } else if (binding.editTextPassword.text.isNullOrEmpty()) {
+//            //if password editText is empty
+//            isLoading(false)
+//            binding.editLayoutEmail.error = null
+//            binding.editLayoutPassword.error = "Please fill the password field"
+//            return false
+//        } else if (binding.editTextPassword.text.toString().length < 6) {
+//            //if password is less than 6 letters, forced by Firebase Authentication Service
+//            isLoading(false)
+//            binding.editLayoutEmail.error = null
+//            binding.editLayoutPassword.error = "Password should contain atleast 6 letters"
+//        }
+//        return true
+//    }
+//
+//    private fun isLoading(bool: Boolean) {
+//        //This is a Rounded progressBar
+//        if(bool) {
+//            binding.progressBar.visibility = View.VISIBLE
+//            binding.buttonSignIn.visibility = View.INVISIBLE
+//        } else {
+//            binding.progressBar.visibility = View.INVISIBLE
+//            binding.buttonSignIn.visibility = View.VISIBLE
+//        }
+//    }
+//
+//    private fun signIn() {
+//        //Trying to Authenticate the user by creating the id
+//        auth.signInWithEmailAndPassword(binding.editTextEmail.text.toString().trim(), binding.editTextPassword.text.toString().trim())
+//            .addOnCompleteListener { task ->
+//                if (task.isSuccessful) {
+//                    isLoading(false)
+//                    startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+//                    finish()
+//                } else {
+//                    isLoading(false)
+//                    showToast("Login failed")
+//                    Log.d("SignIpError",task.exception.toString())
+//                }
+//            }
+//    }
 }
